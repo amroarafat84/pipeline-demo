b3bdcbb4efc1286eb9ee77f40c217fda92e1d066
Amro Arafat <amroarafat84@gmail.com>
2019-10-06 01:18:52 -0400

    change

--- src/main/java/com/infosys/demo11/Demo11Application.java
+++ src/main/java/com/infosys/demo11/Demo11Application.java
@@ -11,7 +11,7 @@ public class Demo11Application {
 
     @GetMapping("/message")
     public String getMessage(){
-        return "Welcome to new Docker with pipeline";
+        return "Welcome to Docker";
     }
 
     public static void main(String[] args) {
